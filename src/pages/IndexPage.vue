c4fb033f6bbff26f2a4f66d73d178f79c60c314b
Montharop <meemontarob@gmail.com>
2023-12-15 10:17:36 +0700

    Changed name from english to thai language

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,11 +8,11 @@
     >
       <q-input
         filled
-        v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        v-model="ชื่อ"
+        label="ชื่อขสกุล *"
+        hint="ให้ใส่ชื่อและนามสกุล"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'กรุณาใส่ชื่อและนามสกุล']"
       />
 
       <q-input
